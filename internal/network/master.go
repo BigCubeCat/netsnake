05c80ce23d351d629478ee0fb6e0f66dda511430
Биточкин Егор <e.bitochkin@g.nsu.ru>
2025-01-21 15:26:31 +0700

    fix(message): пофикисил AnnouncementMessage

---
+++ internal/network/master.go
@@ -0,0 +1,6 @@
+package network
+
+import "github.com/bigcubecat/netsnake/internal/network/message"
+
+func (peer *Peer) SendAnnouncementMsg() {
+}
--- internal/network/message/announcement.go
+++ internal/network/message/announcement.go
@@ -7,18 +7,29 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-func CreateAnnouncementMessage(
+func NewAnnouncementMessage(
 	name, gameName string,
+	width, height, foodStatic, stateDelay int32,
 	role protocol.NodeRole,
+	players *protocol.GamePlayers,
 ) *protocol.GameMessage {
 	return &protocol.GameMessage{
 		MsgSeq: proto.Int64(time.Now().UnixNano()),
 		Type: &protocol.GameMessage_Announcement{
 			Announcement: &protocol.GameMessage_AnnouncementMsg{
-			  Games: []*protocol.GameAnnouncement{
-			    {
-			    }
-			  },
+				Games: []*protocol.GameAnnouncement{
+					{
+						GameName: proto.String(gameName),
+						Config: &protocol.GameConfig{
+							Width:        proto.Int32(width),
+							Height:       proto.Int32(height),
+							FoodStatic:   proto.Int32(foodStatic),
+							StateDelayMs: proto.Int32(stateDelay),
+						},
+						CanJoin: proto.Bool(true),
+						Players: players,
+					},
+				},
 			},
 		},
 	}
